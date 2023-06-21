e29b60d2378b321b8782e156850be02eab1f48c7
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-21 12:15:06 +0900

    {Fix} status를 HttpStatus로

--- src/main/kotlin/nickname/maker/server/global/error/ErrorResponse.kt
+++ src/main/kotlin/nickname/maker/server/global/error/ErrorResponse.kt
@@ -1,9 +1,10 @@
 package nickname.maker.server.global.error
 
 import nickname.maker.server.global.error.exception.ErrorCode
+import org.springframework.http.HttpStatus
 
 class ErrorResponse(
-        val status: Int,
+        val status: HttpStatus,
         val message: String,
 ) {
     constructor(errorCode: ErrorCode): this(errorCode.status, errorCode.message)
--- src/main/kotlin/nickname/maker/server/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/nickname/maker/server/global/error/exception/ErrorCode.kt
@@ -1,20 +1,22 @@
 package nickname.maker.server.global.error.exception
 
+import org.springframework.http.HttpStatus
+
 enum class ErrorCode(
-        val status: Int,
+        val status: HttpStatus,
         val message: String,
 ) {
-    INTERNAL_SERVER_ERROR(500, "서버에 오류가 발생했습니다."),
-    BAD_REQUEST(404, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생했습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 
-    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
-    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
 
-    USER_ALREADY_EXISTS(422, "사용자가 이미 존재합니다."),
-    USER_NOT_FOUND(404, "사용자가 없습니다."),
-    PASSWORD_MISMATCH(401, "비밀번호가 틀렸습니다."),
+    USER_ALREADY_EXISTS(HttpStatus.UNPROCESSABLE_ENTITY, "사용자가 이미 존재합니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자가 없습니다."),
+    PASSWORD_MISMATCH(HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다."),
 
-    PRODUCT_NOT_FOUND(404, "상품이 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품이 없습니다."),
 
-    CART_NOT_FOUND(404, "해당 상품이 장바구니에 담겨있지 않습니다."),
+
 }
