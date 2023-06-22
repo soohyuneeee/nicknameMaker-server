6115fc49ef6739c74d4518d197643ff5c55c6fa7
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-22 12:13:50 +0900

    {Add} JpaConfig

---
+++ src/main/kotlin/nickname/maker/server/global/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package nickname.maker.server.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
